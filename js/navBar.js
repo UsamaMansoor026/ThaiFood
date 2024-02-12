104778c3934ced762f929b43d0c24a904e58d7b3
Usama Mansoor <152704241+UsamaMansoor026@users.noreply.github.com>
2024-02-12 22:21:14 +0500

    Add files via upload

---
+++ js/navBar.js
@@ -0,0 +1,28 @@
+let showNavPhone = true;
+
+/* Function to handle the rendering of NavBar for Phone */
+const handleNavBar = () => {
+  setShowNavPhone(!showNavPhone);
+  if (!showNavPhone) {
+    document.getElementById("navBarForPhone").style.display = "block";
+  } else {
+    document.getElementById("navBarForPhone").style.display = "none";
+  }
+};
+
+/* Function that change the status of showNavPhone */
+const setShowNavPhone = (status) => {
+  showNavPhone = status;
+  return showNavPhone;
+};
+
+/* Function that keep track of the window current Width */
+function handleWindowResize() {
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth > 768) {
+    document.getElementById("navBarForPhone").style.display = "none";
+  }
+}
+/* Event that handle Width change of Window */
+window.addEventListener("resize", handleWindowResize);
